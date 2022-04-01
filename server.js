368ea866739b8dec5755b2daaeea1b7c67214778
ckittiwan <ckittiwan@netizen.co.th>
2022-04-01 19:09:35 +0700

    fix api(/admin) in server.js

--- server.js
+++ server.js
@@ -244,7 +244,7 @@ app.get("/historyuser/:id", (req, res) => {
 
 //list employee(admin) fluke
 //app.use(express.json());
-app.get("/admin/:id", (req, res) => {
+app.get("/admin", (req, res) => {
   //res.send("GET History 111");
   //let id = req.body.id;
   //let id ="ntz";
@@ -255,8 +255,7 @@ app.get("/admin/:id", (req, res) => {
     e.employee_id,e.first_name,e.last_name,h.check_date,h.result
     from m_employee e
     left outer join m_company c on (e.company_id=c.company_id)
-    left outer join t_atk_history h on (e.employee_id=h.employee_id)
-    where e.company_id = '${id}' ORDER BY check_date DESC`;
+    left outer join t_atk_history h on (e.employee_id=h.employee_id) ORDER BY employee_id`;
 
   const connection = new Connection(config);
   connection.on("connect", (err) => {
